767dab1bf87ad6c98dae1a639b4ea09007d23572
labalabadie <labalabadie@gmail.com>
2022-07-17 15:47:43 -0700

    betty ad new gramatica

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,9 +6,10 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (!node)
-		return(0);
-	if (node->parent)
-		return(1);
-	return(0);
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
