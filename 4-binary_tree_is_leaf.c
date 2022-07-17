ecdf267b59d352179527acd58c8e605d0f4cbc3a
labalabadie <labalabadie@gmail.com>
2022-07-17 15:49:01 -0700

    checkeando nueva gramatica

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,9 +6,10 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node)
+	if (node == NULL)
 		return (0);
-	if (!node->right || !node->left)
+	if (node->right != NULL || node->left != NULL)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
