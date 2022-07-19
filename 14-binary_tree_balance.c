fd6c6e9e7eeb63b45e582b118e51369b5ae85d2c
labalabadie <labalabadie@gmail.com>
2022-07-19 10:05:36 -0700

    new aprroach, no longer if statements al pedingui

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,11 +13,31 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		counter_l = 1 + binary_tree_balance(tree->left);
+	counter_l = binary_tree_height(tree->left);
 
-	if (tree->right != NULL)
-		counter_r = 1 + binary_tree_balance(tree->right);
+	counter_r = binary_tree_height(tree->right);
 
 	return (counter_l - counter_r);
+}
+
+/**
+ *binary_tree_height - measures the height of a binary tree
+ *@tree: pointer to root node
+ *Return: 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t counter_r = 0, counter_l = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	counter_l = 1 + binary_tree_height(tree->left);
+
+	counter_r = 1 + binary_tree_height(tree->right);
+
+	if (counter_r > counter_l)
+		return (counter_r);
+	else
+		return (counter_l);
 }
