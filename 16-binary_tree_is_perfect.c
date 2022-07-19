a4b6a98a1cf19eccb940f50d695d1f39ac529c46
FloLys <4440@holbertonschool.com>
2022-07-19 13:11:12 -0700

    16 corrected with height fn

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,7 +17,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (binary_tree_is_perfect(tree->left) &&
 				binary_tree_is_perfect(tree->right));
 
-	return (0);	
+	return (0);
 }
 /**
  *binary_tree_height - measures the height of a binary tree
