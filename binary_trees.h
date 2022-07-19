01c2fb86eda8e45462dee551264ee1c473775d47
labalabadie <labalabadie@gmail.com>
2022-07-19 05:37:55 -0700

    dummy header

--- binary_trees.h
+++ binary_trees.h
@@ -52,4 +52,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+int binary_tree_balance_right(const binary_tree_t *tree);
+int binary_tree_balance_left(const binary_tree_t *tree);
 #endif
