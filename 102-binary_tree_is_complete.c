7073637b26201ee6a63742e2171bbfd4cbb823b1
labalabadie <labalabadie@gmail.com>
2022-07-20 14:07:39 -0700

    new perfect

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -76,6 +76,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (!tree->left && tree-> right)
+		return (0);
+
 	if (!tree->left && !tree->right)
 		return (1);
 
