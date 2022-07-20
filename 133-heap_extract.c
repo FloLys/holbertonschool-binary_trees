16f8077d1c8385885e374f6d19dd7164795aff46
labalabadie <labalabadie@gmail.com>
2022-07-20 08:56:15 -0700

    every file only return NULL

---
+++ 133-heap_extract.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_levelorder - Binary tree travel level-order traversal.
+ *@tree: pointer to root node.
+ *@func: Pointer to function to call in each node.
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+		return;
+}
