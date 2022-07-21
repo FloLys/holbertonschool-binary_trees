2d59f1761c730be99bab98eaaa2d28bc49905e35
labalabadie <labalabadie@gmail.com>
2022-07-20 19:28:49 -0700

    120 incomplete

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,10 +1,43 @@
 #include "binary_trees.h"
+#include <stdio.h>
 /**
- *binary_tree_levelorder - Binary tree travel level-order traversal.
- *@tree: pointer to root node.
- *@func: Pointer to function to call in each node.
+ *binary_tree_is_avl - measures the level balance of a binary tree
+ *@tree: pointer to root node
+ *Return: lvl of balance
  */
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+
+int binary_tree_is_avl(const binary_tree_t *tree)
 {
-		return;
+	int counter_l = 0, counter_r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	counter_l = binary_tree_height(tree->left);
+
+	counter_r = binary_tree_height(tree->right);
+
+	return (counter_l - counter_r);
+}
+
+/**
+ *binary_tree_height - measures the height of a binary tree
+ *@tree: pointer to root node
+ *Return: 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t counter_r = 0, counter_l = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	counter_l = 1 + binary_tree_height(tree->left);
+
+	counter_r = 1 + binary_tree_height(tree->right);
+
+	if (counter_r > counter_l)
+		return (counter_r);
+	else
+		return (counter_l);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -60,6 +60,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_search(const bst_t *tree, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 
 void level_order_traversal_aux(
